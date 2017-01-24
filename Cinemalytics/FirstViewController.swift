395a6eaaaa72a7ced7b8f9ba6a7cc3f8792ae47f
Arthur Melo <arthurvelosogm@icloud.com>
2017-01-24 14:29:21 -0400

    Project created
    Signed-off-by: Arthur Melo <arthurvelosogm@icloud.com>

---
+++ Cinemalytics/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Cinemalytics
+//
+//  Created by Arthur Melo on 24/01/17.
+//  Copyright © 2017 arthurmelo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Cinemalytics/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Cinemalytics
+//
+//  Created by Arthur Melo on 24/01/17.
+//  Copyright © 2017 arthurmelo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
