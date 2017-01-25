092b1060f9d5c8a68e4a3f048101032982a946cd
Arthur Melo <arthurvelosogm@icloud.com>
2017-01-25 14:38:40 -0400

    Released this week screen implemented
    Signed-off-by: Arthur Melo <arthurvelosogm@icloud.com>

--- Cinemalytics/FirstViewController.swift
+++ Cinemalytics/FirstViewController.swift
@@ -15,13 +15,17 @@ class FirstViewController: UIViewController {
     var moviesReleasedNextWeek: [Movies] = []
     var moviesUpcoming: [Movies] = []
     
+    @IBOutlet weak var imageBackground: UIImageView!
+    @IBOutlet weak var movieName: UILabel!
+    @IBOutlet weak var movieGenre: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        moviesReleasedByYear = Movies.getMoviesByYear(year: 2014)
+        //moviesReleasedByYear = Movies.getMoviesByYear(year: 2014)
         moviesReleasedThisWeek = Movies.getMoviesReleasedThisWeek()
-        moviesReleasedNextWeek = Movies.getMoviesNextWeek()
-        moviesUpcoming = Movies.getUpcomingMovies()
+        //moviesReleasedNextWeek = Movies.getMoviesNextWeek()
+        //moviesUpcoming = Movies.getUpcomingMovies()
         //print(moviesReleasedThisWeek[0].title!)
         //print(moviesReleasedNextWeek[0].title!)
         //print(moviesUpcoming[1].title!)
@@ -48,7 +52,29 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+extension FirstViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.moviesReleasedThisWeek.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "week", for: indexPath) as! WeekMoviesCell
+        
+        self.imageBackground.downloadedFrom(link: moviesReleasedThisWeek[indexPath.item].posterPath!)
+        
+        cell.movieImage.adjustsImageWhenAncestorFocused = true
+        
+        cell.movieImage.downloadedFrom(link: moviesReleasedThisWeek[indexPath.item].posterPath!)
+        self.movieName.text = moviesReleasedThisWeek[indexPath.item].title!
+        self.movieGenre.text = moviesReleasedThisWeek[indexPath.item].genre!
+        
+        return cell
+    }
 }
 
---
+++ Cinemalytics/WeekMoviesCell.swift
@@ -0,0 +1,13 @@
+//
+//  WeekMoviesCell.swift
+//  Cinemalytics
+//
+//  Created by Arthur Melo on 25/01/17.
+//  Copyright © 2017 arthurmelo. All rights reserved.
+//
+
+import UIKit
+
+class WeekMoviesCell: UICollectionViewCell {
+    @IBOutlet weak var movieImage: UIImageView!
+}
